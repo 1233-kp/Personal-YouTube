ccece716a2a4c4ce799cc1756d458ae24e938be1
Krishna Pandey <krishnapandey1607@gmail.com>
2026-05-18 17:50:05 +0530

    corrected ApiResponse.js

--- src/utils/ApiResponse.js
+++ src/utils/ApiResponse.js
@@ -1,6 +1,6 @@
 class ApiResponse {
-    constructor(success, message = "Success", data) {
-        this.stausCode = statusCode;
+    constructor(statusCode, message = "Success", data) {
+        this.statusCode = statusCode;
         this.message = message;
         this.data = data;
         this.success = statusCode < 400;
